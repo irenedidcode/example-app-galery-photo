04ba24f1670135a0212c5f017744f51bbab061c8
Ireneee <meitheresha@gmail.com>
2024-12-02 14:03:46 +0700

    pudidi

--- app/Http/Controllers/Admin/NewsPortalController.php
+++ app/Http/Controllers/Admin/NewsPortalController.php
@@ -23,4 +23,33 @@ class NewsPortalController extends Controller
             'newsportals' => Post::with('contents')->get()
         ]);
     }
+
+    public function store(Request $request) {
+        dd($request);
+        /* $validate = $request->validate([
+            'title'     => 'required|unique:posts,title', // Check for unique title
+            'category'  => 'required',
+            'desc'      => 'required',
+            'images'    => 'required',
+            'images.*'  => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ], [
+            'title.required' => 'Nama harus diisi',
+            'title.unique' => 'Judul sudah ada, silakan gunakan judul lain', // Custom error message
+            'desc.required' => 'Di isi ya sayang',
+            'category.required' => 'Isi la panteg',
+            'images.required' => 'Fotonya isi ya',
+        ]);
+
+        // Create post
+        $post = Post::create([
+            'title' => $validate['title'],
+            'category' => $validate['category'],
+            'desc' => $validate['desc'],
+            'slug' => Str::slug($validate['title']),
+            'user_id' => Auth::user()->id,
+        ]); */
+
+        
+        return redirect()->route('admin-newsportal')->with('success', 'News portalz created successfully.');
+    }
 }             
